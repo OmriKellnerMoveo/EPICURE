82c2c7d462aa3dbf8d86d81fad03df48e62d2fb5
OmriKellnerMoveo <omrik@moveo.co.il>
2022-05-03 18:43:05 +0300

    fix carusela and clean commends

--- epicure/src/component/Card/Splide.js
+++ epicure/src/component/Card/Splide.js
@@ -1,61 +1,36 @@
-import React, { useState, useRef } from "react";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
+import React, {useRef} from "react";
+import {Splide, SplideSlide} from "@splidejs/react-splide";
 
 import "@splidejs/splide/dist/css/themes/splide-default.min.css";
 import './Splide.css'
 import Card from "./Card";
 
-export default function(props) {
+export default function (props) {
     const ref = useRef();
     return (
-        <div className={props.type!==3 ?'Splide-container':'Splide-container_type_3'}>
+        <div className={props.type !== 3 ? 'Splide-container' : 'Splide-container_type_3'}>
             <Splide
                 ref={ref}
                 options={{
                     interval: 0,
-                    arrows:false,
+                    arrows: false,
                     type: 'loop',
                     rewind: false,
                     autoScroll: 'false',
                     speed: 2000,
 
-                    autoWidth:false,
+                    autoWidth: false,
                     perPage: 3,
                     perMove: 3,
                     gap: '2vw',
-                    // padding: "3rem",
                     pagination: false,
                     breakpoints: {
                         623: {
-                            // perPage: props.type ===3 ?2:1,
-                            // perMove: props.type ===3 ?2:1,
-
-                            // gap:2
-                            // padding:"10vw",
-                            // gap:"1vw"
+                            perPage: props.type === 3 ? 3 : 2,
+                            perMove: props.type === 3 ? 3 : 2,
                         },
-                        // 935: {
-                        //     perPage: 3,
-                        //     perMove: 3
-                        // },
-                        // 1247: {
-                        //     perPage: 3,
-                        //     perMove: 3
-                        // }
                     }
                 }}
-                // onMounted={() => {
-                //     console.log("mounted");
-                // }}
-                // onUpdated={() => {
-                //     console.log("updated");
-                // }}
-                // onMoved={() => {
-                //     console.log("moved");
-                // }}
-                // onVisible={(splide, slide) => {
-                //     console.log("visible", slide.index);
-                // }}
             >
                 {props.card_list.map(card => {
                     return (
