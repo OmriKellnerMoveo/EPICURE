55e705030498554a34779ec03102304ed531b2dc
OmriKellnerMoveo <omrik@moveo.co.il>
2022-05-08 13:17:14 +0300

    make animation triggers

--- epicure/src/component/Card/Card.js
+++ epicure/src/component/Card/Card.js
@@ -4,31 +4,38 @@ import CardSubTitle from "./CardSubTitle";
 import './Card.css'
 import PriceWithLines from "./PriceWithLines";
 import './../../Animation/tilt-in-top-1.css'
+import {AnimatedOnScroll} from "react-animated-css-onscroll";
 
 const Card = (props) => {
     return (<div>
-        {props.type === 1 && <div className={'Card-Container_type_1 tilt-in-top-1'}>
-            <img className={'image_style_type_1'} src={props.image} />
-            <CardTitle title={props.title} type={1}/>
-            <CardSubTitle type={1} subTitle={props.subTitle}/>
-        </div>}
-        {props.type === 2 && <div>
-            <p className={'top-header'}>{props.topHeader}</p>
-            <div className={'Card-Container_type_2 tilt-in-top-1'}>
-                <img src={props.image}/>
-                <CardTitle title={props.title} type={2}/>
-                <div style={{height: '5vw', marginBottom: '5vw'}}>
-                    <CardSubTitle type={2} subTitle={props.subTitle}/>
-                </div>
-                {props.icon ? <img className={'iconStyle'} src={props.icon} alt='icon'/> :
-                    <div className={'iconStyle'}/>}
-                <PriceWithLines price={props.price}/>
+        {props.type === 1 && <AnimatedOnScroll animationIn="tilt-in-top-1">
+            <div className={'Card-Container_type_1'}>
+                <img className={'image_style_type_1'} src={props.image}/>
+                <CardTitle title={props.title} type={1}/>
+                <CardSubTitle type={1} subTitle={props.subTitle}/>
             </div>
-        </div>}
-        {props.type === 3 && <div className={'Card-Container_type_3 tilt-in-top-1'}>
-            <img src={props.image} />
-            <CardTitle title={props.title} type={3}/>
-        </div>}
+        </AnimatedOnScroll>}
+        {props.type === 2 && <AnimatedOnScroll animationIn="tilt-in-top-1">
+            <div>
+                <p className={'top-header'}>{props.topHeader}</p>
+                <div className={'Card-Container_type_2'}>
+                    <img src={props.image}/>
+                    <CardTitle title={props.title} type={2}/>
+                    <div style={{height: '5vw', marginBottom: '5vw'}}>
+                        <CardSubTitle type={2} subTitle={props.subTitle}/>
+                    </div>
+                    {props.icon ? <img className={'iconStyle'} src={props.icon} alt='icon'/> :
+                        <div className={'iconStyle'}/>}
+                    <PriceWithLines price={props.price}/>
+                </div>
+            </div>
+        </AnimatedOnScroll>}
+        {props.type === 3 && <AnimatedOnScroll animationIn="tilt-in-top-1">
+            <div className={'Card-Container_type_3'}>
+                <img src={props.image}/>
+                <CardTitle title={props.title} type={3}/>
+            </div>
+        </AnimatedOnScroll>}
     </div>)
 
 }
