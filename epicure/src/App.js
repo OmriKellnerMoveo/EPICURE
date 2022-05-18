946cd459eabf9e38c8a1f29f6ad12b556e629430
OmriKellnerMoveo <omrik@moveo.co.il>
2022-05-18 13:50:11 +0300

    add search box

--- epicure/src/App.js
+++ epicure/src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React  from 'react';
 import HomePage from "./Pages/HomePage";
 import FullPage from "./Pages/FullPage";
-
 function App() {
     return (
         <FullPage> <HomePage/></FullPage>
-
     );
 }
 
